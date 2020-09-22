4195e57f70a0a845117a4c9003e833facbf728e9
andreregor <andreregor@gmail.com>
2020-09-22 19:59:10 -0300

    Script que lê a largura e a altura de uma parede em metros, calcula a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m²..

--- exercicio_011.py
+++ exercicio_011.py
@@ -1,6 +1,14 @@
 """
 Faça um programa que leia a largura e a altura de uma parede em metros,
 calcule a sua área e a quantidade de tinta necessária para pintá-la,
-sabendo qye cada litro de tinta pinta uma área de 2 m².
+sabendo que cada litro de tinta pinta uma área de 2 m².
 """
 
+print('Informe as medidas em metros da parede que você quer pintar!')
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+area = larg * alt
+tinta = area / 2
+print()
+print('A sua parede tem {}m² de área.'.format(area))
+print('Para pintar uma parede com essa área, será(ão) necessário(s) {:.2f} litro(s) de tinta.'.format(tinta))
