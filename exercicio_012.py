14e2a9b2148f110e2848e727a70c362a2fc75a2f
andreregor <andreregor@gmail.com>
2020-09-22 20:07:55 -0300

    Script que lê o preço de um produto e mostra seu novo preço, com 5% de desconto.

--- exercicio_012.py
+++ exercicio_012.py
@@ -2,3 +2,8 @@
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 """
 
+preco = float(input('informe o valor do produto: '))
+preco_com_desconto = preco * 0.95
+print(
+    'Você tem direito a um desconto! O produto que você quer levar sairá por apenas R${:.2f}.'
+        .format(preco_com_desconto))
