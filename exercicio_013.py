3f6e4982424303868186c88c82469a16689c07e8
andreregor <andreregor@gmail.com>
2020-09-22 20:13:40 -0300

    Script que lê o salário de um funcionário e mostre seu novo salário com 15% de aumento.

--- exercicio_013.py
+++ exercicio_013.py
@@ -2,3 +2,8 @@
 Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário
 com 15% de aumento.
 """
+
+salario = float(input('Digite o valor do seu salário: '))
+print(
+    'Parabéns!\nComo reconhecimento ao excelente trabalho que você vem desenvolvendo, você recebeu um aumento.'
+    '\nSeu salário agora é de R${:.2f}!'.format(salario * 1.15))
