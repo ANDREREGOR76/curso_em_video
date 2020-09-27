d1dcd6da21361d13dc673e1ddc456f5c068e2719
andreregor <andreregor@gmail.com>
2020-09-27 13:14:49 -0300

    Alterei o tipo das variáveis n1 e n2 de inteiro para ponto flutuante e formatei a saída para ser apresentada a média das notas com apenas uma casa decimal.

--- exercicio_007.py
+++ exercicio_007.py
@@ -2,6 +2,6 @@
 Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 """
 
-n1 = int(input('nota 1: '))
-n2 = int(input('nota 2: '))
-print('A média aritmética de {} e {} é {}'.format(n1, n2, (n1+n2)/2))
+n1 = float(input('nota 1: '))
+n2 = float(input('nota 2: '))
+print('A média aritmética de {} e {} é {:.1f}'.format(n1, n2, (n1+n2)/2))
